cfeca46f3c348883b70df88a52b0899e36890b68
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2023-08-26 01:35:22 -0300

    Inclusão dos métodos de criptografia e descriptografia de textos.

--- src/PasswordCryptography.cs
+++ src/PasswordCryptography.cs
@@ -5,10 +5,99 @@
     using System.Text;
 
     /// <summary>
-    /// Class responsible for functions and methos related to password and cryptography.
+    /// Class responsible for functions and methods related to password and cryptography.
+    /// RB: https://www.c-sharpcorner.com/article/encryption-and-decryption-using-a-symmetric-key-in-c-sharp/.
     /// </summary>
     public static class PasswordCryptography
     {
+        /// <summary>
+        /// Static method that use a symmetric key for decryption.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="encryptedValue"></param>
+        /// <returns>Decrypted value.</returns>
+        public static string DecryptString(string key, string encryptedValue)
+        {
+            try
+            {
+                byte[] iv = new byte[16];   //  Initialization Vector.
+
+                byte[] buffer = Convert.FromBase64String(
+                    encryptedValue);
+
+                using (var aes = Aes.Create())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(
+                        key);
+
+                    aes.IV = iv;
+                    ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                    using (var memoryStream = new MemoryStream(
+                        buffer))
+                    {
+                        using (var cryptoStream = new CryptoStream(
+                            (Stream)memoryStream,
+                            decryptor,
+                            CryptoStreamMode.Read))
+                        {
+                            using (var streamReader = new StreamReader(
+                                (Stream)cryptoStream))
+                            {
+                                return streamReader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Static method that use a symmetric key for encryption.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="normalValue"></param>
+        /// <returns>Encrypted value.</returns>
+        public static string EncryptString(string key, string normalValue)
+        {
+            byte[] iv = new byte[16];   //  Initialization Vector.
+
+            using (var aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(
+                    key);
+
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(
+                    aes.Key,
+                    aes.IV);
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var cryptoStream = new CryptoStream(
+                        (Stream)memoryStream,
+                        encryptor,
+                        CryptoStreamMode.Write))
+                    {
+                        using (var streamWriter = new StreamWriter(
+                            (Stream)cryptoStream))
+                        {
+                            streamWriter.Write(
+                                normalValue);
+                        }
+
+                        return Convert.ToBase64String(
+                            memoryStream.ToArray());
+                    }
+                }
+            }
+        }
+
         /// <summary>
         /// Get hexadecimal string for MD5.
         /// </summary>
