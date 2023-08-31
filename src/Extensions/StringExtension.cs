b4eeb1a47d00cbaf65c454bebb1129914b6324aa
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2023-08-31 00:12:51 -0300

    Inclusão do método Extension de String para TimeSpan.

--- src/Extensions/StringExtension.cs
+++ src/Extensions/StringExtension.cs
@@ -29,5 +29,37 @@
             return iso.GetString(
                 isoBytes);
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static TimeSpan? ToTimeSpan(this string value)
+        {
+            if (string.IsNullOrEmpty(value) || value.Length < 5)
+                return null;
+
+            var numberOfElements = value.Split(':').Length;
+
+            var timeSpan = default(TimeSpan?);
+
+            if (numberOfElements == 3)
+            {
+                timeSpan = new TimeSpan(
+                    int.Parse(value.Split(':')[0]),                 // Hours
+                    int.Parse(value.Split(':')[1]),                 // Minutes
+                    int.Parse(value.Split(':')[2].Split('.')[0]));  // Seconds
+            }
+            else
+            {
+                timeSpan = new TimeSpan(
+                    int.Parse(value.Split(':')[0]),                 // Hours
+                    int.Parse(value.Split(':')[1]),
+                    0);                                             // Minutes
+            }
+
+            return timeSpan;
+        }
     }
 }
