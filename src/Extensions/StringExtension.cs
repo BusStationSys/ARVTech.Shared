2a847107955084167eec84370dbd2eb454d3a05a
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2023-09-03 17:54:15 -0300

    Inclusão do método Capitular (primeira letra em maiúsculo).

--- src/Extensions/StringExtension.cs
+++ src/Extensions/StringExtension.cs
@@ -1,13 +1,35 @@
 ﻿namespace ARVTech.Shared.Extensions
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;
 
     public static class StringExtension
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static string FirstCharToUpper(this string input)
+        {
+            return input switch
+            {
+                null => throw new ArgumentNullException(
+                    nameof(
+                        input)),
+                "" => throw new ArgumentException(
+                    @$"{nameof(
+                        input)} cannot be empty",
+                    nameof(
+                        input)),
+                _ => string.Concat(
+                    input[0].ToString().ToUpper(),
+                    input.Substring(1)),
+            };
+        }
+
         /// <summary>
         /// 
         /// </summary>
