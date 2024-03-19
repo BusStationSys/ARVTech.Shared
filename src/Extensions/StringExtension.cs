10315bb4dbe9a7f498fcf2da5455744ca543c72a
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2024-03-19 01:01:19 -0300

    Implementação do método que substitui todo o tipo de acentuação.

--- src/Extensions/StringExtension.cs
+++ src/Extensions/StringExtension.cs
@@ -6,6 +6,146 @@
 
     public static partial class StringExtension
     {
+        private readonly static Dictionary<string, string> foreign_characters = new()
+        {
+            { "äæǽ", "ae" },
+            { "öœ", "oe" },
+            { "ü", "ue" },
+            { "Ä", "Ae" },
+            { "Ü", "Ue" },
+            { "Ö", "Oe" },
+            { "ÀÁÂÃÄÅǺĀĂĄǍΑΆẢẠẦẪẨẬẰẮẴẲẶА", "A" },
+            { "àáâãåǻāăąǎªαάảạầấẫẩậằắẵẳặа", "a" },
+            { "Б", "B" },
+            { "б", "b" },
+            { "ÇĆĈĊČ", "C" },
+            { "çćĉċč", "c" },
+            { "Д", "D" },
+            { "д", "d" },
+            { "ÐĎĐΔ", "Dj" },
+            { "ðďđδ", "dj" },
+            { "ÈÉÊËĒĔĖĘĚΕΈẼẺẸỀẾỄỂỆЕЭ", "E" },
+            { "èéêëēĕėęěέεẽẻẹềếễểệеэ", "e" },
+            { "Ф", "F" },
+            { "ф", "f" },
+            { "ĜĞĠĢΓГҐ", "G" },
+            { "ĝğġģγгґ", "g" },
+            { "ĤĦ", "H" },
+            { "ĥħ", "h" },
+            { "ÌÍÎÏĨĪĬǏĮİΗΉΊΙΪỈỊИЫ", "I" },
+            { "ìíîïĩīĭǐįıηήίιϊỉịиыї", "i" },
+            { "Ĵ", "J" },
+            { "ĵ", "j" },
+            { "ĶΚК", "K" },
+            { "ķκк", "k" },
+            { "ĹĻĽĿŁΛЛ", "L" },
+            { "ĺļľŀłλл", "l" },
+            { "М", "M" },
+            { "м", "m" },
+            { "ÑŃŅŇΝН", "N" },
+            { "ñńņňŉνн", "n" },
+            { "ÒÓÔÕŌŎǑŐƠØǾΟΌΩΏỎỌỒỐỖỔỘỜỚỠỞỢО", "O" },
+            { "òóôõōŏǒőơøǿºοόωώỏọồốỗổộờớỡởợо", "o" },
+            { "П", "P" },
+            { "п", "p" },
+            { "ŔŖŘΡР", "R" },
+            { "ŕŗřρр", "r" },
+            { "ŚŜŞȘŠΣС", "S" },
+            { "śŝşșšſσςс", "s" },
+            { "ȚŢŤŦτТ", "T" },
+            { "țţťŧт", "t" },
+            { "ÙÚÛŨŪŬŮŰŲƯǓǕǗǙǛŨỦỤỪỨỮỬỰУ", "U" },
+            { "ùúûũūŭůűųưǔǖǘǚǜυύϋủụừứữửựу", "u" },
+            { "ÝŸŶΥΎΫỲỸỶỴЙ", "Y" },
+            { "ýÿŷỳỹỷỵй", "y" },
+            { "В", "V" },
+            { "в", "v" },
+            { "Ŵ", "W" },
+            { "ŵ", "w" },
+            { "ŹŻŽΖЗ", "Z" },
+            { "źżžζз", "z" },
+            { "ÆǼ", "AE" },
+            { "ß", "ss" },
+            { "Ĳ", "IJ" },
+            { "ĳ", "ij" },
+            { "Œ", "OE" },
+            { "ƒ", "f" },
+            { "ξ", "ks" },
+            { "π", "p" },
+            { "β", "v" },
+            { "μ", "m" },
+            { "ψ", "ps" },
+            { "Ё", "Yo" },
+            { "ё", "yo" },
+            { "Є", "Ye" },
+            { "є", "ye" },
+            { "Ї", "Yi" },
+            { "Ж", "Zh" },
+            { "ж", "zh" },
+            { "Х", "Kh" },
+            { "х", "kh" },
+            { "Ц", "Ts" },
+            { "ц", "ts" },
+            { "Ч", "Ch" },
+            { "ч", "ch" },
+            { "Ш", "Sh" },
+            { "ш", "sh" },
+            { "Щ", "Shch" },
+            { "щ", "shch" },
+            { "ЪъЬь", "" },
+            { "Ю", "Yu" },
+            { "ю", "yu" },
+            { "Я", "Ya" },
+            { "я", "ya" },
+        };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static char RemoveDiacritics(this char c)
+        {
+            foreach (KeyValuePair<string, string> entry in foreign_characters)
+            {
+                if (entry.Key.Contains(c))
+                    return entry.Value[0];
+            }
+
+            return c;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string RemoveDiacritics(this string s)
+        {
+            var sbText = new StringBuilder(); 
+
+            foreach (char c in s)
+            {
+                int len = sbText.Length;
+
+                foreach (KeyValuePair<string, string> entry in foreign_characters)
+                {
+                    if (entry.Key.Contains(c))
+                    {
+                        sbText.Append(
+                            entry.Value);
+                        break;
+                    }
+                }
+
+                if (len == sbText.Length)
+                    sbText.Append(
+                        c);
+            }
+
+            return sbText.ToString();
+        }
+
         /// <summary>
         /// 
         /// </summary>
