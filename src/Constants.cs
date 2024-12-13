b62572e7b5e836059e43e8dde770d4fa731b3c02
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2024-12-13 18:42:47 -0300

    Atualização de submódulo.

--- src/Constants.cs
+++ src/Constants.cs
@@ -14,5 +14,165 @@
         /// Holds the pattern used in email`s Regex.
         /// </summary>
         public const string PatternRegexEmail = @"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasCalculos = "C";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasEventos = "E";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasMatriculas = "M";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasMatriculasDemonstrativosPagamento = "MDP";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasMatriculasDemonstrativosPagamentoEventos = "MDPE";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasMatriculasDemonstrativosPagamentoTotalizadores = "MDPT";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasMatriculasEspelhosPonto = "MEP";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasMatriculasEspelhosPontoCalculos = "MEPC";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasMatriculasEspelhosPontoMarcacoes = "MEPM";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasPessoas = "P";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasPessoasFisicas = "PF";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasPessoasJuridicas = "PJ";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasPublicacoes = "PU";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasTotalizadores = "T";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasUnidadesNegocio = "UN";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableAliasUsuarios = "U";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameCalculos = "CALCULOS";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameEventos = "EVENTOS";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameMatriculas = "MATRICULAS";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameMatriculasDemonstrativosPagamento = "MATRICULAS_DEMONSTRATIVOS_PAGAMENTO";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameMatriculasDemonstrativosPagamentoEventos = "MATRICULAS_DEMONSTRATIVOS_PAGAMENTO_EVENTOS";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameMatriculasDemonstrativosPagamentoTotalizadores = "MATRICULAS_DEMONSTRATIVOS_PAGAMENTO_TOTALIZADORES";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameMatriculasEspelhosPonto = "MATRICULAS_ESPELHOS_PONTO";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameMatriculasEspelhosPontoCalculos = "MATRICULAS_ESPELHOS_PONTO_CALCULOS";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameMatriculasEspelhosPontoMarcacoes = "MATRICULAS_ESPELHOS_PONTO_MARCACOES";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNamePessoas = "PESSOAS";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNamePessoasFisicas = "PESSOAS_FISICAS";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNamePessoasJuridicas = "PESSOAS_JURIDICAS";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNamePublicacoes = "PUBLICACOES";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameUsuarios = "USUARIOS";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameTotalizadores = "TOTALIZADORES";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TableNameUnidadesNegocio = "UNIDADES_NEGOCIO";
     }
 }
