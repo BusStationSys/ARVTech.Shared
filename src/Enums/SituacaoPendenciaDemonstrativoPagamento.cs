2766bbc7343c048b2a0c42efc34e3bc61cb1cad8
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2025-10-02 00:07:58 -0300

    Unificação da enum de Situação da Pendência de DP.

---
+++ src/Enums/SituacaoPendenciaDemonstrativoPagamento.cs
@@ -0,0 +1,14 @@
+﻿namespace ARVTech.Shared.Enums
+{
+    /// <summary>
+    /// Enumm with values "Situação Pendência Demonstrativo Pagamento".
+    /// </summary>
+    public enum SituacaoPendenciaDemonstrativoPagamento
+    {
+        Todos,
+
+        Cientes,
+
+        Pendentes,
+    }
+}
---
+++ src/Extensions/EnumExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace ARVTech.Shared.Extensions
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class EnumExtension
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            var member = value.GetType().GetMember(value.ToString()).FirstOrDefault();
+            var attribute = member?.GetCustomAttribute<DisplayAttribute>();
+            return attribute?.GetName() ?? value.ToString();
+        }
+    }
+}
