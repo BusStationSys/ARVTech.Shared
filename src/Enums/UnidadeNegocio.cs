83f01b0a30a987e853c1e4353a18e1757e7fc123
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2025-10-14 00:02:31 -0300

    Inclusão da Enum de Unidade de Negócio.

---
+++ src/Enums/UnidadeNegocio.cs
@@ -0,0 +1,37 @@
+﻿namespace ARVTech.Shared.Enums
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Enum with values that represents "Unidade de Negócio".
+    /// </summary>
+    public enum UnidadeNegocio
+    {
+        [Display(Name = "TODAS")]
+        Todas,
+
+        [Display(Name = "MATRIZ")]
+        Matriz = 1,
+
+        [Display(Name = "MACROMIX")]
+        Macromix = 2,
+
+        [Display(Name = "RISSUL")]
+        Rissul = 3,
+
+        [Display(Name = "ATACAREJO")]
+        Atacarejo = 4,
+
+        [Display(Name = "INDÚSTRIA")]
+        Industria = 5,
+
+        [Display(Name = "LOGÍSTICA")]
+        Logistica = 6,
+
+        [Display(Name = "MANUTENÇÃO")]
+        Manutencao = 7,
+
+        [Display(Name = "TRANSPORTE")]
+        Transporte = 8,
+    }
+}
